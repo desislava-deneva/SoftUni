760f67ed8497ad9c59231dc3f60425b7c044c630
desislava-deneva <dessy8905@gmail.com>
2022-02-15 10:15:29 +0200

    addGift

--- JS Advanced 01.2022/Exams/01. Christmas Gifts Delivery_Ресурси (1)/app.js
+++ JS Advanced 01.2022/Exams/01. Christmas Gifts Delivery_Ресурси (1)/app.js
@@ -1,73 +1,47 @@
 function solution() {
-    let [sectionAdd, sectionListOfGift, sectionSend, sectionDiscarded] = document.querySelectorAll('section');
+    let [sectionAddGift,sectionListOfGift,sectionSendGift, sectionDiscardedGift ] =  document.querySelectorAll('section');
 
-    let input = sectionAdd.querySelector('input');
-    let btnAdd = sectionAdd.querySelector('button');
+    let input = sectionAddGift.querySelector('input');
+    let btnAddGift = sectionAddGift.querySelector('button');
 
-    btnAdd.addEventListener('click', addGift);
-    sectionListOfGift.addEventListener('click', onClick)
+    let ulListGift = sectionListOfGift.querySelector('ul');
+    let ulSendGift = sectionSendGift.querySelector('ul');
+    let ulDiscardedGift = sectionDiscardedGift.querySelector('ul');
+
+    btnAddGift.addEventListener('click', addGift)
 
     function addGift(ev) {
         ev.preventDefault();
 
-        if (input.value == '') {
-            return;
-        }
-
-        let listOfGiftUlEl = sectionListOfGift.querySelector('ul');
-
-        let li = createElement('li', `${input.value}`, 'gift', listOfGiftUlEl);
-
-        let sendBtn = createElement('button', `Send`, '', li);
-        sendBtn.setAttribute('id', 'sendButton');
-
-        let discardBtn = createElement('button', `Discard`, '', li);
-        discardBtn.setAttribute('id', 'discardButton');
-
-        let liElements = Array.from(listOfGiftUlEl.querySelectorAll('li'));
-
-        sortElements(liElements, listOfGiftUlEl);
-
-        input.value = '';
-
-    }
-
-    function onClick(ev) {
-        ev.preventDefault();
-
-        let currEvent = ev.target;
-        let buttons = Array.from(currEvent.parentElement.querySelectorAll('button'));
-
-        if (currEvent.textContent == 'Send') {
-
-            let sendGiftsUl = sectionSend.querySelector('ul');
-            sendGiftsUl.appendChild(currEvent.parentElement);
-
-            removeElements(buttons);
-
-            let sendGiftsLis = Array.from(sectionSend.querySelectorAll('li'));
-            sortElements(sendGiftsLis, sendGiftsUl);
-
-        }else if(currEvent.textContent == 'Discard'){
-
-            let discardUlEl = sectionDiscarded.querySelector('ul');
-            discardUlEl.appendChild(currEvent.parentElement);
-
-            removeElements(buttons);
-
-            let discardLi = Array.from(discardUlEl.querySelectorAll('li'))
-            sortElements(discardLi, discardUlEl);
+        if (input.value != '') {
+            let li = createElement('li', `${input.value}`, 'gift', ulListGift);
+            let btnSend = createElement('button', 'Send', '', li);
+            btnSend.id = 'sendButton';
             
+            let btnDiscard = createElement('button', 'Discard', '', li);
+            btnDiscard.id = 'discardButton';
+
+        Array.from(ulListGift.querySelectorAll('li'))
+        .sort((a,b)=> a.textContent.localeCompare(b.textContent))
+        .forEach(li => ulListGift.appendChild(li));
+
+            btnSend.addEventListener('click', (ev)=>{
+                moveOnClick(ev, ulSendGift)
+            });
+
+            btnDiscard.addEventListener('click', (ev)=>{
+                moveOnClick(ev,ulDiscardedGift);
+            });
+
+            input.value = '';
         }
     }
 
-    function sortElements(arr, appender) {
-        arr.sort((a, b) => a.textContent.localeCompare(b.textContent)).forEach(li => appender.appendChild(li))
-
-    }
-
-    function removeElements(arr) {
-        arr.filter(el => el.remove());
+    function moveOnClick(ev, appender){
+        let liList = ev.target.parentElement;
+        liList.querySelector('button').remove();
+        liList.querySelector('button').remove();
+        appender.appendChild(liList);
     }
 
     function createElement(type, content, attribute, appender) {
