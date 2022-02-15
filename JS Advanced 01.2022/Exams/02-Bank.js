ed6b2a5cc98f5ab9ea4a5938439b24dd9af93fdc
desislava-deneva <dessy8905@gmail.com>
2022-02-15 10:06:20 +0200

    Add jsApp

--- JS Advanced 01.2022/Exams/02-Bank.js
+++ JS Advanced 01.2022/Exams/02-Bank.js
@@ -1,82 +1,84 @@
 class Bank {
     constructor(bankName) {
-        this.bankName = bankName;
+        this._bankName = bankName;
         this.allCustomers = [];
-        this.transaction = {};
     }
 
-    newCustomer({firstName, lastName, personalId}) {
-        let person = this.allCustomers.find(o => o.firstName == firstName && o.lastName == lastName);
+    newCustomer(customer) {
+        let { firstName, lastName, personalId } = customer;
+        personalId = Number(personalId);
+        let person = this.allCustomers.find(o => o.personalId === personalId && o.firstName === firstName &&o.lastName === lastName);
+
         if (person) {
-            return `${firstName} ${lastName} is already our customer!`
+            throw new Error(`${firstName} ${lastName} is already our customer!`);
         }
+
         this.allCustomers.push({ firstName, lastName, personalId });
 
-        return { firstName, lastName, personalId };
-
+        return customer;
     }
 
     depositMoney(personalId, amount) {
+        personalId = Number(personalId);
         amount = Number(amount);
-        let person = this.allCustomers.find(o => o.personalId == personalId);
+        let index;
+        let person = this.allCustomers.find(o => o.personalId === personalId);
 
         if (!person) {
-            return `We have no customer with this ID!`;
+            throw new Error`We have no customer with this ID!`;
         }
-        if (person && !person.hasOwnProperty('totalMoney')) {
+
+        if (!person.hasOwnProperty('totalMoney')) {
             person.totalMoney = 0;
-            this.transaction[person.personalId] = [];
+            person.transaction = [];
         }
+        index = person.transaction.length;
+
         person.totalMoney += amount;
-        this.transaction[person.personalId].push(`${person.firstName} ${person.lastName} made deposit of ${amount}$!`);
-        return `${person.totalMoney}$`;
+        person.transaction.push(`${index + 1}. ${person.firstName} ${person.lastName} made deposit of ${amount}$!`);
+        return `${person.totalMoney}$`
     }
 
     withdrawMoney(personalId, amount) {
+        personalId = Number(personalId);
         amount = Number(amount);
-        let person = this.allCustomers.find(o => o.personalId == personalId);
 
+        let person = this.allCustomers.find(o => o.personalId === personalId);
         if (!person) {
-            return `We have no customer with this ID!`;
+            throw new Error('We have no customer with this ID!');
         }
 
-        if (person.totalMoney - amount < 0) {
-            throw new Error(`${person.firstName} ${person.lastName} does not have enough money to withdraw that amount!`)
+        if (person.totalMoney  < amount && person.hasOwnProperty('totalMoney')) {
+            throw new Error(`${person.firstName} ${person.lastName} does not have enough money to withdraw that amount!`);
         }
+
         person.totalMoney -= amount;
-        this.transaction[person.personalId].push(`${person.firstName} ${person.lastName} withdrew ${amount}$!`);
-
-        return `${person.totalMoney}$`;
+        person.transaction.push(`${person.transaction.length + 1}. ${person.firstName} ${person.lastName} withdrew ${amount}$!`)
+        return `${person.totalMoney}$`
     }
 
     customerInfo(personalId) {
-        let person = this.allCustomers.find(o => o.personalId == personalId);
+        personalId = Number(personalId);
         let result = [];
+        let person = this.allCustomers.find(d => d.personalId === personalId);
+
         if (!person) {
-            return `We have no customer with this ID!`;
+            throw new Error('We have no customer with this ID!');
         }
 
-        result.push(`Bank name: ${this.bankName}`);
+        result.push(`Bank name: ${this._bankName}`);
         result.push(`Customer name: ${person.firstName} ${person.lastName}`);
-        result.push(`Customer ID: ${person.personalId}`);
+        result.push(`Customer ID: ${personalId}`);
         result.push(`Total Money: ${person.totalMoney}$`);
         result.push(`Transactions:`);
-        let index = this.transaction[personalId].length
-        this.transaction[personalId].reverse();
-        this.transaction[personalId].forEach(line => {
-            result.push(`${index}. ${line}`);
-            index--;
-        });
 
-        return result.join('\n')
-
+        let reverse = person.transaction.reverse();
+        for (const line of reverse) {
+            result.push(line);
+        }
+        return result.join('\n');
     }
-
 }
-
-//Unexpected error: Incorrect output: expected 'Bank name: SoftUni Bank\nCustomer name: Svetlin Nakov\nCustomer ID: 1111111\nTotal Money: 375$\nTransactions:\n3. Svetlin Nakov withdrew 125$!\n2. Svetlin Nakov made deposit of 250$!\n1. Svetlin Nakov made deposit of 250$!'
-//                                    to equal 'Bank name: SoftUni Bank\nCustomer name: Svetlin Nakov\nCustomer ID: 1111111\nTotal Money: 375$\nTransactions:\n3. Svetlin Nakov withdrew 125$!\n2. Svetlin Nakov made depostit of 250$!\n1. Svetlin Nakov made depostit of 250$!'
-
 
 let bank = new Bank("SoftUni Bank");
 
